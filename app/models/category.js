794cf471f132173dfb61e582732abe00249110d0
Adanetx <Adane1301992@gmail.com>
2020-09-09 17:43:03 -0500

    update after lost

---
+++ app/models/category.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const User = require('./user')
+
+const Schema = mongoose.Schema
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
+})
+
+const Category = mongoose.model('Post', categorySchema)
+
+module.exports = Category
